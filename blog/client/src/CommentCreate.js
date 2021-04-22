6c9390573317c814b1f6215747aacbbbe858737b
Fiqri Syah Redha <wibulo27@gmail.com>
2021-04-22 22:09:06 +0700

    feat(blog): add add comment and comments list

---
+++ blog/client/src/CommentCreate.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group mb-2">
+          <label>New Comments</label>
+          <input
+            className="form-control"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
