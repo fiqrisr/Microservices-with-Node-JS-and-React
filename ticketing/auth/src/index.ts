29bc3e897a0522e4a0d89665603ab86c6d8a2371
Fiqri Syah Redha <wibulo27@gmail.com>
2021-04-27 00:45:26 +0700

    feat(ticketing): add error handler

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import { errorHandler } from './middlewares/error-handler';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import { currentUserRouter } from './routes/current-user';
+import { NotFoundError } from './errors/not-found';
 
 const app = express();
 app.use(express.json());
@@ -12,5 +14,11 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 app.use(currentUserRouter);
+
+app.all('*', () => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => console.log('Listening on 3000'));
