5c550b2161d3ca2506e0cc79b7eccaf39d573476
Fiqri Syah Redha <wibulo27@gmail.com>
2021-04-28 23:13:08 +0700

    feat(ticketing): add user schema

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 
 import { errorHandler } from './middlewares/error-handler';
 import { signinRouter } from './routes/signin';
@@ -22,4 +23,20 @@ app.all('*', () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => console.log('Listening on 3000'));
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		});
+
+		console.log('Connected to database');
+	} catch (err) {
+		console.error(err);
+	}
+
+	app.listen(3000, () => console.log('Listening on 3000'));
+};
+
+start();
