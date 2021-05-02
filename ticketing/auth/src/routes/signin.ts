b547c81f209b80ecb11a965d6b21bdc8b9aedfb6
Fiqri Syah Redha <wibulo27@gmail.com>
2021-05-02 22:45:43 +0700

    feat(ticketing): add sign in

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
+import { Password } from '../services/password';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -11,7 +16,33 @@ router.post(
 		body('password').trim().notEmpty().withMessage('You must suply a password')
 	],
 	validateRequest,
-	async (req: Request, res: Response) => {}
+	async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+
+		const existingUser = await User.findOne({ email });
+
+		if (!existingUser) throw new BadRequestError('Invalid credentials');
+
+		const passwordMatch = await Password.compare(existingUser.password, password);
+
+		if (!passwordMatch) throw new BadRequestError('Invalid credentials');
+
+		// Generate JWT
+		const userJwt = jwt.sign(
+			{
+				id: existingUser.id,
+				email: existingUser.email
+			},
+			process.env.JWT_KEY as string
+		);
+
+		// Store it on session object
+		req.session = {
+			jwt: userJwt
+		};
+
+		res.status(200).send(existingUser);
+	}
 );
 
 export { router as signinRouter };
