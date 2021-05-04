8f051dca57407355d405c3fd82a049e110a96ace
Fiqri Syah Redha <wibulo27@gmail.com>
2021-05-04 23:37:49 +0700

    refactor(ticketing): seperate script to start app

---
+++ ticketing/auth/src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+
+import { errorHandler } from './middlewares/error-handler';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { currentUserRouter } from './routes/current-user';
+import { NotFoundError } from './errors/not-found';
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(express.json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: true
+	})
+);
+
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+app.use(currentUserRouter);
+
+app.all('*', () => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,36 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { errorHandler } from './middlewares/error-handler';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { currentUserRouter } from './routes/current-user';
-import { NotFoundError } from './errors/not-found';
-
-const app = express();
-app.set('trust proxy', true);
-
-app.use(express.json());
-app.use(
-	cookieSession({
-		signed: false,
-		secure: true
-	})
-);
-
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-app.use(currentUserRouter);
-
-app.all('*', () => {
-	throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
 	if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined!');
