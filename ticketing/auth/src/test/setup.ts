bc326e085c1cacb1074431b0438868080a672e45
Fiqri Syah Redha <wibulo27@gmail.com>
2021-05-05 01:21:44 +0700

    refactor(ticketing): refactor test

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,7 +1,17 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 
 import { app } from '../app';
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace NodeJS {
+		interface Global {
+			signin(): Promise<string[]>;
+		}
+	}
+}
 
 let mongo: MongoMemoryServer;
 
@@ -27,3 +37,16 @@ afterAll(async () => {
 	await mongo.stop();
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const email = 'test@test.com';
+	const password = 'password';
+
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send({ email, password })
+		.expect(201);
+
+	const cookie = response.get('Set-Cookie');
+	return cookie;
+};
