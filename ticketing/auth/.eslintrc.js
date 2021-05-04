273c9ce1ae58c1a0d9055084689e382994078d87
Fiqri Syah Redha <wibulo27@gmail.com>
2021-05-05 02:02:43 +0700

    feat(ticketing): init client app

--- ticketing/auth/.eslintrc.js
+++ ticketing/auth/.eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
-  },
+	env: {
+		commonjs: true,
+		es2021: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'prettier'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module'
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'prettier/prettier': ['error', { endOfLine: 'auto' }]
+	}
 };
