72564fcdc6d9552cebfb6bf3c911225cf3d921ae
Fiqri Syah Redha <wibulo27@gmail.com>
2021-06-30 23:51:22 +0700

    fix(ticketing): request from server

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,61 +1,49 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/useRequest';
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const { doRequest, errors } = useRequest({
+		url: '/api/users/signup',
+		method: 'post',
+		body: { email, password },
+		onSuccess: () => Router.push('/')
+	});
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+	const onSubmit = async e => {
+		e.preventDefault();
 
-    try {
-      await axios.post("/api/users/signup", {
-        email,
-        password,
-      });
+		doRequest();
+	};
 
-      setErrors([]);
-    } catch (err) {
-      setErrors(err.response.data.errors);
-    }
-  };
-
-  return (
-    <form onSubmit={onSubmit}>
-      <h1>Sign Up</h1>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input
-          className="form-control"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          className="form-control"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ooops...</h4>
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <button className="btn btn-primary">Sign Up</button>
-    </form>
-  );
+	return (
+		<form onSubmit={onSubmit}>
+			<h1>Sign Up</h1>
+			<div className="form-group">
+				<label>Email Address</label>
+				<input
+					className="form-control"
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+					required
+				/>
+			</div>
+			<div className="form-group">
+				<label>Password</label>
+				<input
+					className="form-control"
+					type="password"
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+					required
+				/>
+			</div>
+			{errors}
+			<button className="btn btn-primary">Sign Up</button>
+		</form>
+	);
 };
 
 export default Signup;
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,23 @@
-const Home = () => <h1>Home</h1>;
+import axios from 'axios';
+
+const Home = ({ currentUser }) => {
+	console.log(currentUser);
+	return <h1>Home</h1>;
+};
+
+Home.getInitialProps = async ({ req }) => {
+	if (typeof window === 'undefined') {
+		const { data } = await axios.get(
+			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+			{
+				headers: req.headers
+			}
+		);
+		return data;
+	} else {
+		const { data } = await axios.get('/api/users/currentuser');
+		return data;
+	}
+};
 
 export default Home;
