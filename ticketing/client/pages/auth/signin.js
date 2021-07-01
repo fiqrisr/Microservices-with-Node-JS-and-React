b0ac4550cfa992a85d8bac7b3b9262a7f629df5d
Fiqri Syah Redha <wibulo27@gmail.com>
2021-07-01 14:55:02 +0700

    feat(ticketing): add header to client

--- ticketing/client/pages/auth/signin.js
+++ ticketing/client/pages/auth/signin.js
@@ -3,11 +3,11 @@ import Router from 'next/router';
 import useRequest from '../../hooks/useRequest';
 import PageWrapper from '../../components/PageWrapper';
 
-const Signup = () => {
+const Signin = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const { doRequest, errors } = useRequest({
-		url: '/api/users/signup',
+		url: '/api/users/signin',
 		method: 'post',
 		body: { email, password },
 		onSuccess: () => Router.push('/')
@@ -22,7 +22,7 @@ const Signup = () => {
 	return (
 		<PageWrapper>
 			<form onSubmit={onSubmit}>
-				<h1>Sign Up</h1>
+				<h1>Sign In</h1>
 				<div className="form-group">
 					<label>Email Address</label>
 					<input
@@ -43,10 +43,10 @@ const Signup = () => {
 					/>
 				</div>
 				{errors}
-				<button className="btn btn-primary">Sign Up</button>
+				<button className="btn btn-primary">Sign In</button>
 			</form>
 		</PageWrapper>
 	);
 };
 
-export default Signup;
+export default Signin;
