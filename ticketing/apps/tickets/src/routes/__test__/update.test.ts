f254231ad0c103ab88a29a9468f90efd6d65792a
Fiqri Syah Redha <wibulo27@gmail.com>
2023-02-16 23:21:59 +0700

    add nats wrapper mocks

--- ticketing/apps/tickets/src/routes/__test__/update.test.ts
+++ ticketing/apps/tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { natsWrapper } from '../../__mocks__/nats-wrapper';
 
 it('returns a 404 if the provided id is not exist', async () => {
 	const id = new mongoose.Types.ObjectId().toHexString();
@@ -91,4 +92,24 @@ it('updates the ticket provided with valid inputs', async () => {
 
 	expect(ticketResponse.body.title).toEqual('new title');
 	expect(ticketResponse.body.price).toEqual(30);
+});
+
+it('publishes an event', async () => {
+	const cookie = global.signin();
+
+	const response = await request(app).post('/api/tickets').set('Cookie', cookie).send({
+		title: 'dasjf9u3r',
+		price: 20
+	});
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title: 'new title',
+			price: 30
+		})
+		.expect(200);
+
+	expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
--- ticketing/apps/tickets/src/test/setup.ts
+++ ticketing/apps/tickets/src/test/setup.ts
@@ -21,6 +21,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+	jest.clearAllMocks();
 	const collections = await mongoose.connection.db.collections();
 
 	collections.forEach(async collection => await collection.deleteMany({}));
