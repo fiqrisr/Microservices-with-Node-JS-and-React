980bde494dc906afc0eed8d0bba3957689ff8f8d
Fiqri Syah Redha <wibulo27@gmail.com>
2023-02-22 23:37:34 +0700

    feat(ticketing:order): add test for new order route

--- ticketing/apps/orders/src/routes/new.ts
+++ ticketing/apps/orders/src/routes/new.ts
@@ -1,10 +1,15 @@
-import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
-import { NotFoundError, requireAuth, validateRequest, BadRequestError } from '@wnlz/common';
+import express, { Request, Response } from 'express';
+import {
+	requireAuth,
+	validateRequest,
+	NotFoundError,
+	OrderStatus,
+	BadRequestError
+} from '@wnlz/common';
 import { body } from 'express-validator';
-
 import { Ticket } from '../models/ticket';
-import { Order, OrderStatus } from '../models/order';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -24,28 +29,35 @@ router.post(
 	async (req: Request, res: Response) => {
 		const { ticketId } = req.body;
 
+		// Find the ticket the user is trying to order in the database
 		const ticket = await Ticket.findById(ticketId);
+		if (!ticket) {
+			throw new NotFoundError();
+		}
 
-		if (!ticket) throw new NotFoundError();
-
+		// Make sure that this ticket is not already reserved
 		const isReserved = await ticket.isReserved();
+		if (isReserved) {
+			throw new BadRequestError('Ticket is already reserved');
+		}
 
-		if (isReserved) throw new BadRequestError('Ticket is already reserved');
-
+		// Calculate an expiration date for this order
 		const expiration = new Date();
 		expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
+		// Build the order and save it to the database
 		const order = Order.build({
 			userId: req.currentUser!.id,
 			status: OrderStatus.Created,
 			expiresAt: expiration,
 			ticket
 		});
-
 		await order.save();
+
+		// Publish an event saying that an order was created
 
 		res.status(201).send(order);
 	}
 );
 
-export { router as createOrderRouter };
+export { router as newOrderRouter };
